9f8f4b647fef8efc3ef5243f80f42b6237252183
Hector Romojaro <hector.romojaro@gmail.com>
2018-03-27 14:22:30 +0000

    Fix typos

--- tcl/dotlrn-news-aggregator-procs.tcl
+++ tcl/dotlrn-news-aggregator-procs.tcl
@@ -46,7 +46,7 @@ ad_proc -public dotlrn_news_aggregator::remove_applet {} {
 ad_proc -public dotlrn_news_aggregator::add_applet_to_community {
     community_id
 } {
-    Add the news-aggregator applet to a specifc dotlrn community
+    Add the news-aggregator applet to a specific dotlrn community
 } {
     set portal_id [dotlrn_community::get_portal_id -community_id $community_id]
     
@@ -93,7 +93,7 @@ ad_proc -public dotlrn_news_aggregator::remove_applet_from_community {
 } {
     remove the applet from the community
 } {
-    ad_return_complaint 1 "[applet_key] remove_applet_from_community not implimented!"
+    ad_return_complaint 1 "[applet_key] remove_applet_from_community not implemented!"
 }
 
 ad_proc -public dotlrn_news_aggregator::add_user {
@@ -115,7 +115,7 @@ ad_proc -public dotlrn_news_aggregator::add_user_to_community {
     community_id
     user_id
 } {
-    Add a user to a specifc dotlrn community
+    Add a user to a specific dotlrn community
 } {
     set package_id [dotlrn_community::get_applet_package_id -community_id $community_id -applet_key [applet_key]]
     set portal_id [dotlrn::get_portal_id -user_id $user_id]
